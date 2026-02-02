52571fc5288cb7a09816a046b7cd159566a3de19
Krishnendu Narayan <57310676+Krishnenu@users.noreply.github.com>
2026-02-02 14:01:17 +0530

    hoisting concept

---
+++ Concepts/JavaScript/concepts/hoisting.js
@@ -0,0 +1,40 @@
+// var keyowrd:
+
+// console.log(a);
+// a = 10;
+// console.log(a);
+// var a;
+// console.log(a);
+
+// var foo;
+// console.log(foo);
+// foo = 1;
+// console.log(foo);
+
+// let const
+// y;
+// let y = "local";
+
+// Foo;
+
+// class Foo {
+//   constructor() {}
+// }
+
+// console.log(bar);
+
+var bar = function () {
+  console.log("BARRRR");
+};
+
+bar();
+// console.log(foo);
+// foo();
+
+function foo() {
+  console.log("FOOOOO");
+}
+
+// foo.doSomething(); // Works normally.
+
+// import foo from "./modules/foo";
